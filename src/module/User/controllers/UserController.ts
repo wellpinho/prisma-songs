28509e478fc7129204b407a18ee0be050433f84f
wellpinho <wellpinho@outlook.com>
2022-02-01 21:13:11 -0300

    ✨ NEW FEATURE: create an user api

--- src/module/User/controllers/UserController.ts
+++ src/module/User/controllers/UserController.ts
@@ -7,4 +7,27 @@ export class UserController {
 
     return res.status(200).json(users)
   }
+
+  async create(req: Request, res: Response) {
+    const { name, email } = req.body
+
+    const userExists = await prismaClient.user.findUnique({
+      where: {
+        email
+      }
+    })
+
+    if (userExists) {
+      return res.status(401).json({ Error: 'User already exists!'})
+    }
+
+    const user = await prismaClient.user.create({
+      data: {
+        name,
+        email
+      }
+    })
+
+    return res.status(201).json(user)
+  }
 }
--- src/module/User/routes/User.routes.ts
+++ src/module/User/routes/User.routes.ts
@@ -5,5 +5,6 @@ const userRoutes = Router()
 const userController = new UserController()
 
 userRoutes.get('/users', userController.list)
+userRoutes.post('/users', userController.create)
 
 export { userRoutes }
